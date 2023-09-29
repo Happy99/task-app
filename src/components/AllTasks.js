3b3e9f19530585434e50719e7b3f383b952805ee
Petr Šťastný <petr.stastny99@gmail.com>
2023-09-29 11:16:28 +0200

    final

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -1,20 +1,34 @@
 import "./AllTasks.css"
 import data from "../data"
+import { useState } from "react"
 
 import Task from "./Task"
 
 const AllTasks = () => {
+    const [myTasks, setMyTasks] = useState(data)
+
+    const deleteAllTasksHanlder = () => {
+        setMyTasks([])
+    }
+
     return (
         <div className="all-tasks-wrapper">
             {
-                data.map((oneTask) => {         
+                myTasks.map((oneTask) => {         
                     const {id, name} = oneTask
                     
                     return (
-                        <Task key={id} name={name}/>
+                        <Task 
+                            key={id} 
+                            name={name} 
+                            id={id} 
+                            myTasks={myTasks} 
+                            setMyTasks={setMyTasks}
+                        />                    
                     )         
                 })
             }
+            <button type="submit" className="delete-all" onClick={deleteAllTasksHanlder}>Delete all</button>
         </div>
     )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,19 @@
 import "./Task.css"
 
 const Task = (props) => {
+
+    const deleteTaskHandler = (taskId) => {
+        const filteredTasks = props.myTasks.filter( (task) => {
+            return (task.id !== taskId)
+        })
+
+        props.setMyTasks(filteredTasks)  
+    }
+
     return (
-        <div  className="oneTask">
+        <div className="oneTask">
             <h4>{props.name}</h4>
+            <button type="button" onClick={ () => deleteTaskHandler(props.id) }>Vymazat</button>
         </div>
     )
 }
